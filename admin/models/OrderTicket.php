df6d5c7d46ba11d7cc16bab5acf18c7c174acd9a
baobao199 <nguyenbao605@gmail.com>
2020-04-23 20:27:34 +0700

    book ticket

---
+++ admin/models/OrderTicket.php
@@ -0,0 +1,25 @@
+<?php
+	class OrderTicket{
+		public $id;
+		public $dateT;
+		public $total;
+		public $status;
+
+
+		/**
+		 * Class Constructor
+		 * @param    $id   
+		 * @param    $dateT   
+		 * @param    $total   
+		 * @param    $status   
+		 */
+		public function __construct($id, $dateT, $total, $status)
+		{
+			$this->id = $id;
+			$this->dateT = $dateT;
+			$this->total = $total;
+			$this->status = $status;
+		}
+		
+	}
+?>
