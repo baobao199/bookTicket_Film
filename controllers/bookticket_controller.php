e52e3e37199741b7def9aece59857386b7298152
baobao199 <nguyenbao605@gmail.com>
2020-05-06 16:35:02 +0700

    bookticket

--- controllers/bookticket_controller.php
+++ controllers/bookticket_controller.php
@@ -1,6 +1,8 @@
 <?php
 	require_once("base_controller.php");
 	require_once("models/TicketManager.php");
+	require_once("models/FilmManager.php");
+	require_once("models/Food.php");
 	require_once("function.php");
 	class BookticketController extends BaseController{
 		function __construct()
@@ -13,7 +15,9 @@
 		function index()
 		{
 			$nameTicket = TicketManager::getAll();
-			$this->render('index', array('nameticket'=>$nameTicket));
+			$nameFilm = FilmManager::getAll();
+			$nameFood = Food::getAll();
+			$this->render('index', array('nameticket'=>$nameTicket, 'namefilm'=>$nameFilm, 'namefood'=>$nameFood));
 		}
 	}
 ?>
--- views/bookticket/index.php
+++ views/bookticket/index.php
@@ -1,68 +1,119 @@
+<link rel="stylesheet" type="text/css" href="css/content.css">
 <link rel="stylesheet" type="text/css" href="css/bookticket.css">
-<form>
-	<h5>Chọn Rạp Chiếu Phim</h5>
-			<div class="custom-select" style="width: 600px;">
-					<select>
-						<option value="0">Chọn Rạp</option>
-						<option value="1">CGV Hùng Vương Plaza</option>
-						<option value="2">CGV Vincom Thủ Đức</option>
-						<option value="3">CGV Crescent Mall</option>
-						<option value="4">CGV Thảo Điền Pearl</option>
-						<option value="5">CGV Vincom Thủ Đức</option>
-						<option value="6">CGV Vivo City</option>
-						<option value="7">CGV Pearl Plaza</option>
-						<option value="8">CGV Liberty Citypoint</option>
-						<option value="9">CGV Vincom Đồng Khởi</option>
-						<option value="10">CGV Trường Sơn (CGV CT Plaza)</option>
-						<option value="11">CGV Pandora City</option>
-						<option value="12">CGV Vincom Gò Vấp</option>
-						<option value="13">CGV Hoàng Văn Thụ</option>
-						<option value="14">CGV Aeon Bình Tân</option>
-						<option value="15">CGV Saigonres Nguyễn Xí</option>
-						<option value="16">CGV Parkson Đồng Khởi</option>
-						<option value="17">CGV Sư Vạn Hạnh</option>
-						<option value="18">CGV IMC Trần Quang Khải</option>
-						<option value="19">CGV Vincom Center Landmark 81</option>
-						<option value="20">CGV Satra Củ Chi</option>
-						<option value="21">CGV Lý Chính Thắng</option>
-					</select>
-		</div>
-		<h5>Chọn Loại vé</h5>
-		<div class="custom-select" style="width: 600px;">
-				<select>
-          <option value="0">Chọn Loại Vé</option>
-          <?php
-            foreach ($nameticket as $t) {
-              ?>
-                <option value="<?= $t->id ?>"><?= $t->name ?></option>
-              <?php
-            }
-          ?>
-				</select>
-		</div>
-		<h5>Chọn Giờ chiếu</h5>
-		<div class="custom-select" style="width: 600px;">
-				<select>
-					<option value="0">Chọn giờ</option>
-          <option value="9:30">9:30</option>
-          <option value="10:30">10:30</option>
-          <option value="11:30">11:30</option>
-          <option value="13:30">13:30</option>
-          <option value="14:30">14:30</option>
-          <option value="15:30">15:30</option>
-          <option value="16:30">16:30</option>
-          <option value="17:30">17:30</option>
-          <option value="18:30">18:30</option>
-          <option value="19:30">19:30</option>
-          <option value="20:30">20:30</option>
-          <option value="21:30">21:30</option>
-          <option value="22:30">22:30</option>
-          <option value="23:30">23:30</option>
-				</select>
-		</div>
-		<br>
-		<a href="chooseSite.html" class="btn btn-danger active">Chọn ghế</a>
-</form>
+
+<style>
+  h4{
+    font-weight: bold;
+  }
+  .content{
+    padding: 20px;
+    padding-left: 100px;
+  }
+  h6{
+    font-weight: bold;
+  }
+  .custom-select{
+    margin-bottom: 20px;
+  }
+</style>
+
+<div class='hot col-12'>
+  <h4><hr>ĐẶT VÉ XEM PHIM<hr></h4> 
+</div>
+<div class="content">
+  <form>
+    <h6>Chọn phim</h6>
+      <div class="custom-select" style="width: 600px;">
+          <select>
+            <option value="0">Chọn phim</option>
+            <?php
+              foreach ($namefilm as $f) {
+                ?>
+                  <option value="<?= $f->id ?>"><?= $f->name ?></option>
+                <?php
+              }
+            ?>
+          </select>
+      </div>
+  	<h6>Chọn Rạp Chiếu Phim</h6>
+  			<div class="custom-select" style="width: 600px;">
+  					<select>
+  						<option value="0">Chọn Rạp</option>
+  						<option value="1">CGV Hùng Vương Plaza</option>
+  						<option value="2">CGV Vincom Thủ Đức</option>
+  						<option value="3">CGV Crescent Mall</option>
+  						<option value="4">CGV Thảo Điền Pearl</option>
+  						<option value="5">CGV Vincom Thủ Đức</option>
+  						<option value="6">CGV Vivo City</option>
+  						<option value="7">CGV Pearl Plaza</option>
+  						<option value="8">CGV Liberty Citypoint</option>
+  						<option value="9">CGV Vincom Đồng Khởi</option>
+  						<option value="10">CGV Trường Sơn (CGV CT Plaza)</option>
+  						<option value="11">CGV Pandora City</option>
+  						<option value="12">CGV Vincom Gò Vấp</option>
+  						<option value="13">CGV Hoàng Văn Thụ</option>
+  						<option value="14">CGV Aeon Bình Tân</option>
+  						<option value="15">CGV Saigonres Nguyễn Xí</option>
+  						<option value="16">CGV Parkson Đồng Khởi</option>
+  						<option value="17">CGV Sư Vạn Hạnh</option>
+  						<option value="18">CGV IMC Trần Quang Khải</option>
+  						<option value="19">CGV Vincom Center Landmark 81</option>
+  						<option value="20">CGV Satra Củ Chi</option>
+  						<option value="21">CGV Lý Chính Thắng</option>
+  					</select>
+  		</div>
+  		<h6>Chọn Loại vé</h6>
+  		<div class="custom-select" style="width: 600px;">
+  				<select>
+            <option value="0">Chọn Loại Vé</option>
+            <?php
+              foreach ($nameticket as $t) {
+                ?>
+                  <option value="<?= $t->id ?>"><?= $t->name ?></option>
+                <?php
+              }
+            ?>
+  				</select>
+  		</div>
+  		<h6>Chọn Giờ chiếu</h6>
+  		<div class="custom-select" style="width: 600px;">
+  				<select>
+  					<option value="0">Chọn giờ</option>
+            <option value="9:30">9:30</option>
+            <option value="10:30">10:30</option>
+            <option value="11:30">11:30</option>
+            <option value="13:30">13:30</option>
+            <option value="14:30">14:30</option>
+            <option value="15:30">15:30</option>
+            <option value="16:30">16:30</option>
+            <option value="17:30">17:30</option>
+            <option value="18:30">18:30</option>
+            <option value="19:30">19:30</option>
+            <option value="20:30">20:30</option>
+            <option value="21:30">21:30</option>
+            <option value="22:30">22:30</option>
+            <option value="23:30">23:30</option>
+  				</select>
+  		</div>
+
+      <h6>Chọn Đồ Ăn</h6>
+      <div class="custom-select" style="width: 600px;">
+          <select>
+            <option value="0">Chọn đồ ăn</option>
+            <?php
+              foreach ($namefood as $f) {
+                ?>
+                  <option value="<?= $f->idFood ?>"><?= $f->nameFood ?></option>
+                <?php
+              }
+            ?>
+          </select>
+      </div>
+  		<br>
+  		<a href="chooseSite.html" class="btn btn-danger active">Chọn ghế</a>
+  </form>
+</div>
+
 <script>
             var x, i, j, selElmnt, a, b, c;
 /*look for any elements with the class "custom-select":*/
