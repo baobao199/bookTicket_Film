25b921927fcfdafc2d5240b77944ea9c93282473
baobao199 <nguyenbao605@gmail.com>
2020-05-09 13:11:16 +0700

    detail book

--- controllers/bookticket_controller.php
+++ controllers/bookticket_controller.php
@@ -7,6 +7,7 @@
 	require_once("models/Food.php");
 	require_once("models/ShowTime.php");
 	require_once("models/Account.php");
+	require_once("models/BookTicket.php");
 	require_once("function.php");
 	class BookticketController extends BaseController{
 		function __construct()
@@ -32,67 +33,80 @@
 
 		function bookticket(){
 
+			//tạo số hóa đơn đặt vé
 			$idBookTicket = BookTicketDetail::getIdBookTicket();
 
-			for($i=0; $i<count($idBookTicket); $i++){
-				$valueLast = $idBookTicket[$i]['maDatVe'];
+			if( empty($idBookTicket) ){
+				$billNumber = 'VCM00001';
+			}
+			else{
+				for($i=0; $i<count($idBookTicket); $i++){
+					$valueLast = $idBookTicket[$i]['maDatVe'];
+				}
+				$number = substr( $valueLast, -5);//lấy số hóa đơn
+				$billNumber = idNumber($number);
 			}
 
-			$number = substr( $valueLast, -5);
+			//Lấy tên người đặt
+			$acc = unserialize($_SESSION['acc']);
+			$nameCustomer = $acc->fullName;
 
-			echo idNumber($number);
+			//Kiểm tra
+			$idmovieTheater = filter_input(INPUT_POST,'movietheater',FILTER_SANITIZE_STRING);
+			$idFilm = filter_input(INPUT_POST,'namefilm',FILTER_SANITIZE_STRING);
+			$dateF = filter_input(INPUT_POST,'datef',FILTER_SANITIZE_STRING);
+			$timeF = filter_input(INPUT_POST,'timef',FILTER_SANITIZE_STRING);
+			$idTicket = filter_input(INPUT_POST,'idticket',FILTER_SANITIZE_STRING);
+			$quantityTicket = filter_input(INPUT_POST,'quantityticket',FILTER_SANITIZE_STRING);
+			$idFood = filter_input(INPUT_POST,'idfood',FILTER_SANITIZE_STRING);
+			$quantityFood = filter_input(INPUT_POST,'quantityfood',FILTER_SANITIZE_STRING);
 
-			
-			// $acc = unserialize($_SESSION['acc']);
+			$ticket = TicketManager::getTicketById($idTicket); //lấy ds vé 
+			$food = Food::getFoodById($idFood); //lấy ds thức ăn
+			$film = FilmManager::getFilmById($idFilm); //lấy ds phim
+			$movieTheater = MovieTheater::getMovieTheaterById($idmovieTheater); //lấy ds rạp phim
 
-			// $nameCustomer = $acc->fullName;
-
-			// $idmovieTheater = filter_input(INPUT_POST,'movietheater',FILTER_SANITIZE_STRING);
-			// $idFilm = filter_input(INPUT_POST,'namefilm',FILTER_SANITIZE_STRING);
-			// $dateF = filter_input(INPUT_POST,'datef',FILTER_SANITIZE_STRING);
-			// $timeF = filter_input(INPUT_POST,'timef',FILTER_SANITIZE_STRING);
-			// $idTicket = filter_input(INPUT_POST,'idticket',FILTER_SANITIZE_STRING);
-			// $quantityTicket = filter_input(INPUT_POST,'quantityticket',FILTER_SANITIZE_STRING);
-			// $idFood = filter_input(INPUT_POST,'idfood',FILTER_SANITIZE_STRING);
-			// $quantityFood = filter_input(INPUT_POST,'quantityfood',FILTER_SANITIZE_STRING);
-
-			// $ticket = TicketManager::getTicketById($idTicket); //lấy ds vé 
-			// $food = Food::getFoodById($idFood); //lấy ds thức ăn
-			// $film = FilmManager::getFilmById($idFilm); //lấy ds phim
-			// $movieTheater = MovieTheater::getMovieTheaterById($idmovieTheater); //lấy ds rạp phim
-
-			// foreach ($ticket as $t) {
-			// 	$priceTicket = $t->price; //lấy giá vé
-			// }
-			// $aumountTicket = TicketManager::sumPrice($priceTicket, $quantityTicket);// thành tiền giá vé
+			foreach ($ticket as $t) {
+				$priceTicket = $t->price; //lấy giá vé
+			}
+			$aumountTicket = TicketManager::sumPrice($priceTicket, $quantityTicket);// thành tiền giá vé
 
 
-			// foreach ($food as $f) {
-			// 	$nameFood = $f->nameFood;
-			// 	$priceFood = $f->price; //lấy giá đồ ăn
-			// }
-			// $amountFood = TicketManager::sumPrice($priceFood, $quantityFood);//thành tiền đồ ăn
+			foreach ($food as $f) {
+				$nameFood = $f->nameFood;
+				$priceFood = $f->price; //lấy giá đồ ăn
+			}
+			$amountFood = TicketManager::sumPrice($priceFood, $quantityFood);//thành tiền đồ ăn
 
-			// foreach ($film as $n) {
-			// 	$nameFilm = $n->name; //lấy tên phim
-			// }
+			foreach ($film as $n) {
+				$nameFilm = $n->name; //lấy tên phim
+			}
 
-			// foreach ($movieTheater as $m) {
-			// 	$nameMovieTheater = $m->name; //lấy tên rạp phim
-			// }
+			foreach ($movieTheater as $m) {
+				$nameMovieTheater = $m->name; //lấy tên rạp phim
+			}
 
-			// $total_price = $aumountTicket + $amountFood; //tính tổng tiền
+			$total_price = $aumountTicket + $amountFood; //tính tổng tiền
 
+			//lấy ngày hiện tại
+			$now = new DateTime();
+			$nowDate = $now->format('Y-m-d'); 
 
-			// BookTicketDetail::addBookTicket('VCM00002',$nameCustomer, $nameMovieTheater, $nameFilm, $dateF, $timeF, $idTicket, $quantityTicket, $priceTicket, $nameFood, $quantityFood, $priceFood, $total_price);
+			//thêm vào đặt vé
+			BookTicket::addBookTicket($billNumber, $nameCustomer, $nowDate, $total_price, 'No approved');
 
-			// // $this->render('bookticket',array());
+			//thêm vào chi tiết đặt vé
+			BookTicketDetail::addBookTicket($billNumber,$nameCustomer, $nameMovieTheater, $nameFilm, $dateF, $timeF, $idTicket, $quantityTicket, $priceTicket, $nameFood, $quantityFood, $priceFood, $total_price);
 
-			// redirect("?controller=bookticket&action=detail");// xem thông tin vé
+			$this->render('bookticket',array());
+
+			echo $billNumber;
+
+			//redirect("?controller=bookticket&action=detail");// xem thông tin vé
 		}
 
 		function detail(){
-			$orderDetail = BookTicketDetail::getBookTicketById();
+			
 			$this->render('detail',array('orderdetail'=>$orderDetail));
 		}
 	}
---
+++ models/BookTicket.php
@@ -0,0 +1,50 @@
+<?php
+	 class BookTicket{
+	 	public $id;
+	 	public $customer;
+	 	public $dateBook;
+	 	public $total;
+	 	public $status;
+
+
+		/**
+		 * Class Constructor
+		 * @param    $id   
+		 * @param    $customer   
+		 * @param    $dateBook   
+		 * @param    $total   
+		 * @param    $status   
+		 */
+		public function __construct($id, $customer, $dateBook, $total, $status)
+		{
+			$this->id = $id;
+			$this->customer = $customer;
+			$this->dateBook = $dateBook;
+			$this->total = $total;
+			$this->status = $status;
+		}
+
+		public function addBookTicket($id, $customer, $dateBook, $total, $status){
+			$sql = "INSERT INTO datVe VALUES ( :id, :customer, :dateBook, :total, :status)";
+			$db = DB::getDB();
+			$stm = $db->prepare($sql);
+
+			$stm->execute(array(':id'=>$id, ':customer'=>$customer, ':dateBook'=>$dateBook, ':total'=>$total, ':status'=>$status));
+
+			return $stm->rowCount() == 1;
+		}
+
+		public function getBookTicketByNameCustomer($nameCustomer){
+			$sql = "SELECT * from datve where khachHang = :nameCustomer";
+			$db = DB::getDB();
+			$stm = $db->prepare($sql);
+			$stm->execute(array('id'=> $id));
+			$list = array();
+			foreach ($stm->fetchAll(PDO::FETCH_ASSOC) as $item) 
+			{
+				$list[]	= new BookTicket($item['maDatVe'], $item['khachHang'], $item['ngayDat'], $item['tongTien'], $item['tinhTrang']);		
+			}
+			return $list;
+		}
+	}
+?>
